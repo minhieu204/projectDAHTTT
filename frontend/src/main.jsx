35f3471bd3a4d39f1dce1de9b5981a24fb9d724a
minhieu204 <minhhieu13062004@gmail.com>
2025-08-27 18:04:23 +0700

    install mui

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.jsx'
+import CssBaseline from '@mui/material/CssBaseline'
+import theme from './theme'
+import { ThemeProvider } from '@mui/material'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <ThemeProvider
+      theme={theme}
+      disableTransitionOnChange>
+      <CssBaseline enableColorScheme/>
+      <App />
+    </ThemeProvider>
   </StrictMode>,
 )
