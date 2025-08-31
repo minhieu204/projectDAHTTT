18d3d8570280c218b271b302e7b9ed6f0a4c0709
minhieu204 <minhhieu13062004@gmail.com>
2025-08-31 14:47:08 +0700

    app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,12 @@
 import Button from '@mui/material/Button'
 import HomePage from './pages/HomePage/HomePage'
+import { Box, Container } from '@mui/material'
 function App() {
 
   return (
-    <>
+    <Container>
       <HomePage />
-    </>
+    </Container>
   )
 }
 
