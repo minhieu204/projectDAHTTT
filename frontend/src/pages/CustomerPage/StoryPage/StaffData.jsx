039efbf436d9b6fd6ca1be4d1a42dcdf7f988fd4
Nguyễn Công Toàn <ncthb24@gmail.com>
2025-09-29 16:20:48 +0700

    storypage

---
+++ frontend/src/pages/CustomerPage/StoryPage/StaffData.jsx
@@ -0,0 +1,47 @@
+export const StaffData = [
+  {
+    id: 1,
+    name: 'Ba-Cao-Thi-Ngoc-Dung',
+    Image: 'https://res.cloudinary.com/dj53ibv5n/image/upload/v1758726246/1-BA-CAO-THI-NGOC-DUNG_ppgmsl.jpg'
+  },
+  {
+    id: 2,
+    name: 'Ong-Le-Tri-Thong',
+    Image: 'https://res.cloudinary.com/dj53ibv5n/image/upload/v1758726247/2-LTT_iruopd.png'
+  },
+  {
+    id: 3,
+    name: 'Ba-Tran-Phuong-Ngoc-Thao',
+    Image: 'https://res.cloudinary.com/dj53ibv5n/image/upload/v1758726247/3-TNPT_xxgmq8.png'
+  },
+  {
+    id: 4,
+    name: 'Ong-Dao-Trung-Kien',
+    Image: 'https://res.cloudinary.com/dj53ibv5n/image/upload/v1758726247/4-ONG-DAO-TRUNG-KIEN_mj666f.jpg'
+  },
+  {
+    id: 5,
+    name: 'Ong-Le-Quang-Phuc',
+    Image: 'https://res.cloudinary.com/dj53ibv5n/image/upload/v1758726247/5-ONG-LE-QUANG-PHUC_wo8zdf.jpg'
+  },
+  {
+    id: 6,
+    name: 'Ba-Dang-Thi-lai',
+    Image: 'https://res.cloudinary.com/dj53ibv5n/image/upload/v1758726247/6-BA-DANG-THI-LAI_qtse1q.jpg'
+  },
+  {
+    id: 7,
+    name: 'Ong-Dang-Hai-Anh',
+    Image: 'https://res.cloudinary.com/dj53ibv5n/image/upload/v1758726248/7-DHA_w1w1kc.png'
+  },
+  {
+    id: 8,
+    name: 'Ba-Tieu-Yen-Trinh',
+    Image: 'https://res.cloudinary.com/dj53ibv5n/image/upload/v1758726248/8-BA-TIEU-YEN-TRINH-ENG2_yeriij.jpg'
+  },
+  {
+    id: 9,
+    name: 'Ong-Nguyen-Tuan-Hai',
+    Image: 'https://res.cloudinary.com/dj53ibv5n/image/upload/v1758726248/9-NTH-EN_qggbkx.png'
+  }
+]
